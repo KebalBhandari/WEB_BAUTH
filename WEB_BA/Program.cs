14d52a393072b674cd68b815e3c4cbadde63bf51
kebal <kebal@LENOVO>
2024-10-30 11:48:47 +0100

    Add project files.

---
+++ WEB_BA/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Configuration;
+
+namespace WEB_BA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ WEB_BA/Startup.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace WEB_BA
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+            });
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+               .AddCookie(options =>
+               {
+                   options.Cookie.Expiration = TimeSpan.FromMinutes(10);
+               });
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/SessionExpired";
+                options.SlidingExpiration = true;
+            });
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/ExceptionError");
+            }
+            else if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/ExceptionError");
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/PageNotFound";
+                    await next();
+                }
+            });
+
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Login}/{action=Index}/{id?}");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=PageNotFound}/{action=index}");
+            });
+        }
+    }
+}
