d4b023aaedc5cd59bc6881e8f3bde3e9ec354821
Kebal Bhandari <kebalbhandari@outlook.com>
2024-12-12 09:34:31 +0100

    Cleanup Login

--- WEB_BA/Controllers/LoginController.cs
+++ WEB_BA/Controllers/LoginController.cs
@@ -2,18 +2,11 @@
 using Microsoft.Data.SqlClient;
 using WEB_BA.DataProvider;
 using WEB_BA.Models;
-using WEB_BA.Services;
 
 namespace WEB_BA.Controllers
 {
     public class LoginController : Controller
     {
-        //private readonly FirebaseService _firebaseService;
-
-        //public LoginController(FirebaseService firebaseService)
-        //{
-        //    _firebaseService = firebaseService;
-        //}
 
         private readonly IConfiguration _configuration;
 
@@ -91,52 +84,6 @@ namespace WEB_BA.Controllers
                 return View(model);
             }
         }
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Index(LoginModel model)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        TempData["msgtype"] = "error";
-        //        TempData["message"] = "Please correct the errors and try again.";
-        //        return View(model);
-        //    }
-        //    try
-        //    {
-        //        var idToken = await _firebaseService.LoginWithEmailAndPasswordAsync(model.Email, model.Password);
-        //        if (idToken is null)
-        //        {
-        //            TempData["msgtype"] = "info";
-        //            TempData["message"] = "Login Failed, Try Again !!!";
-        //            return View();
-        //        }
-        //        else
-        //        {
-        //            var userProfile = _firebaseService.DecodeIdToken(idToken);
-
-        //            HttpContext.Session.SetString("TokenNo", idToken);
-        //            HttpContext.Session.SetString("UserName", userProfile.DisplayName ?? "");
-        //            HttpContext.Session.SetString("UserEmail", userProfile.Email ?? "");
-
-        //            TempData["msgtype"] = "LoginSuccess";
-        //            TempData["message"] = "Login Successfull !!!";
-        //            return RedirectToAction("Index", "Dashboard");
-        //        }
-        //    }
-        //    catch (FirebaseAuthenticationException ex)
-        //    {
-        //        TempData["msgtype"] = "info";
-        //        TempData["message"] = ex.FirebaseMessage;
-        //        return View();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        TempData["msgtype"] = "info";
-        //        TempData["message"] = ex.ToString();
-        //        return View();
-        //    }
-        //}
     }
 }
 
