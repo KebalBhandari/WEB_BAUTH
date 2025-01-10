c98e9f5dc98887a7ecec3bdf1800ea762c3ec6ce
Kebal Bhandari <kebalbhandari@outlook.com>
2025-01-10 22:12:33 +0100

    Update Test Modal

---
+++ WEB_BA/Controllers/TestModelController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_BA.Controllers
+{
+    public class TestModelController : Controller
+    {
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index()
+        {
+            try
+            {
+                string session = HttpContext.Session.GetString("TokenNo");
+                if (session == null || session == "")
+                {
+                    return RedirectToAction("Index", "SessionExpired");
+                }
+                else
+                {
+                    var UserName = HttpContext.Session.GetString("UserName");
+                    if (UserName == "" || UserName == null)
+                    {
+                        TempData["UserName"] = "Welcome";
+                    }
+                    else
+                    {
+                        TempData["UserName"] = UserName;
+                    }
+                    ViewBag.msgtype = "";
+                    ViewBag.message = "";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                string Exception = ex.ToString();
+                TempData["Exception"] = Exception;
+                return RedirectToAction("Index", "UnexpectedError");
+            }
+        }
+    }
+}
